cd1665849cefea3dc9f1857f5bb3865dffa176ea
Hari Sekhon <harisekhon@gmail.com>
2026-01-08 02:33:09 -0500

    updated watcher.lua

--- audio/watcher.lua
+++ audio/watcher.lua
@@ -56,7 +56,8 @@ hs.audiodevice.watcher.setCallback(function(_, _)
             else
                 log.d("Debounced, skipping switch")
             end
-        elseif current:match("Speakers.*Blackhole") or current:match("Multi%-Output Device") then
+        elseif current:match("Speakers.*Blackhole")
+            or current:match("Multi%-Output Device") then
             local now = hs.timer.secondsSinceEpoch()
             if now - last_switch > debounce_time then
                 last_switch = now
