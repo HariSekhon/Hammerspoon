d62d727fe42c15f063fc1b359a1ec7f2204b589c
Hari Sekhon <harisekhon@gmail.com>
2026-01-08 00:49:32 -0500

    added watcher.lua

---
+++ audio/watcher.lua
@@ -0,0 +1,51 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2025-10-28 20:33:02 +0300 (Tue, 28 Oct 2025)
+--
+--  vim:ts=4:sts=4:sw=4:et
+--
+--  https///github.com/HariSekhon/Hammerspoon
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn
+--  and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/HariSekhon
+--
+
+-- ========================================================================== --
+--                      Hammerspoon Audio Change Watcher
+-- ========================================================================== --
+
+-- luacheck: globals hs switchInputToMacMic switchInputToBlackhole switchOutputToMultiDevice
+
+--local audioSwitchLog = hs.logger.new('audioSwitch', 'info')
+local log = hs.logger.new("audioWatcher", "info")
+
+local last_switch = 0
+local debounce_time = 1  -- seconds
+
+hs.audiodevice.watcher.setCallback(function(_, _)
+    local current = hs.audiodevice.defaultOutputDevice():name()
+    print("Audio event:", current)
+
+    if not current:match("AirPods") then
+        return
+    end
+
+    local now = hs.timer.secondsSinceEpoch()
+    if now - last_switch <= debounce_time then
+        log.d("Debounced, skipping switch")
+        return
+    end
+
+    last_switch = now
+    log.d("Debounce OK, switching audio")
+
+    -- allow macOS to settle
+    hs.timer.doAfter(0.5, switchInputToBlackhole)
+    hs.timer.doAfter(0.5, switchOutputToMultiDevice)
+end)
+
+hs.audiodevice.watcher.start()
