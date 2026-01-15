4308b21402ac6b46e63ace1f295dc2c15165c002
Hari Sekhon <harisekhon@gmail.com>
2026-01-14 22:49:59 -0500

    updated watcher-disk-space.lua

--- transmission/watcher-disk-space.lua
+++ transmission/watcher-disk-space.lua
@@ -53,5 +53,5 @@ local function quit_transmission_if_disk_space_low()
     end
 end
 
-log "Starting Transmission Watcher: Disk Space"
 hs.timer.doEvery(check_interval_secs, quit_transmission_if_disk_space_low)
+log "Watcher Started: Transmission - Disk Space"
